a8bc00ea9561e77b72b046c95c79b4a4404b6788
ToshikBusa <toshik.busa.97@gmail.com>
2019-07-06 02:03:44 +0700

    Aditii

---
+++ aditii/assets/scripts/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+    $(".navbar-toggler").click(function() {
+        $(".navbar-toggler-icon").toggleClass("active");
+    });
+
+    $('.multiple-items').slick({
+    	arrows: false,
+    	autoplay: true,
+	    dots: true,
+		infinite: true,
+		slidesToShow: 1,
+		slidesToScroll: 1
+	});
+
+    $('.multiple-items-3').slick({
+        arrows: true,
+        autoplay: true,
+        dots: false,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-prev"><img src="assets/images/arrow_prev.png" alt="" /></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="assets/images/arrow_next.png" alt="" /></button>',
+        responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                arrows: false,
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                arrows: false,
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+        ]
+    });
+
+});
