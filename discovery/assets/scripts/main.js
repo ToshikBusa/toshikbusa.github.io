63976e3eddd938c1b2dace485a16622f239b9395
ToshikBusa <toshik.busa.97@gmail.com>
2019-07-09 19:55:23 +0700

    Discovery

--- discovery/assets/scripts/main.js
+++ discovery/assets/scripts/main.js
@@ -13,4 +13,23 @@ $(document).ready(function() {
 		slidesToScroll: 1
 	});
 
+    $("div.lazy").lazyload({
+        effect : "fadeIn"
+    });
+
+    $('#pagepiling').pagepiling({
+        verticalCentered: true,
+        css3: false,
+        onLeave: function (index, nextIndex, direction) {
+            
+            if (nextIndex == 4) {
+                $('#pp-nav').fadeOut();
+            }
+ 
+            if (index == 4) {
+                $('#pp-nav').fadeIn();
+            }
+        },
+    });
+
 });
